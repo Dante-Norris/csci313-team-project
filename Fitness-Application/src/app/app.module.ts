d6935b02f7b86b6e31f0833c8fbdcf3f38f63577
Dante Norris <dante.norris@ndus.edu>
2019-05-02 22:50:53 -0500

    Data base
    Connected firebase database to the project

--- Fitness-Application/src/app/app.module.ts
+++ Fitness-Application/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,10 @@ import { ProfileComponent } from './profile/profile.component';
 import { ExerciseComponent } from './exercise/exercise.component';
 import { MealPlannerComponent } from './diet/meal-planner.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +23,11 @@ import { MealPlannerComponent } from './diet/meal-planner.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Fitness-Application/src/environments/environment.ts
+++ Fitness-Application/src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAE_I6MIbYII48cyHiplYTu8bGYgpptiOo",
+    authDomain: "csci-313-team-project.firebaseapp.com",
+    databaseURL: "https://csci-313-team-project.firebaseio.com",
+    projectId: "csci-313-team-project",
+    storageBucket: "csci-313-team-project.appspot.com",
+    messagingSenderId: "159312083367"
+  }
 
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
