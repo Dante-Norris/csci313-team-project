096e0d11ec407c91fd3a518714c5b579000767fd
Dante Norris <dante.norris@ndus.edu>
2019-05-04 11:49:48 -0500

    More work on the login/register components

--- Fitness-Application/src/app/app-routing.module.ts
+++ Fitness-Application/src/app/app-routing.module.ts
@@ -4,12 +4,21 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ExerciseComponent } from './exercise/exercise.component';
 import { MealPlannerComponent } from './diet/meal-planner.component';
+
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { RegisterComponentComponent } from './register-component/register-component.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'exercise', component: ExerciseComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'meal-planner', component: MealPlannerComponent },
+
+
+  { path: 'login', component: LoginComponentComponent },
+  { path: 'regsiter', component: RegisterComponentComponent },
+
 
 ];
 
--- Fitness-Application/src/app/services/auth.service.ts
+++ Fitness-Application/src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private user: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(private _firebaseAuth: AngularFireAuth, private router: Router) {
+    this.user = _firebaseAuth.authState;
+  }
 }
