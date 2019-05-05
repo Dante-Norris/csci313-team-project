70e25d8618ca5adb49c5751c5c9bc1c438a027a4
Dante Norris <dante.norris@ndus.edu>
2019-05-05 14:19:56 -0500

    Login/Logout and Register are functionally done, just need flair

--- Fitness-Application/src/app/home/home.component.ts
+++ Fitness-Application/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../providers/auth.service'
-
+import * as firebase from 'firebase/app';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,7 +10,14 @@ export class HomeComponent implements OnInit {
 
   constructor(private authService: AuthService) { }
 
-
+  getLoggedStatus() {
+    if(firebase.auth().currentUser) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
   logout() {
     this.authService.logout();
   }
